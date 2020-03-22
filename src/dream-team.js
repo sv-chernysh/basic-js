350c1cf3c11f5b55ff1ea811d3755ed9790786a4
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 19:33:17 +0200

    feat: finish task dream team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,11 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!Array.isArray(members)) return false;
+  let titleOfTeam = '';
+  for (let i = 0; i < members.length; i++) {
+    if (typeof members[i] === 'string') {
+      titleOfTeam += members[i].trim().substr(0, 1).toUpperCase();
+    }
+  }
+    titleOfTeam = titleOfTeam.split('').sort().join('');
+    return titleOfTeam
 };

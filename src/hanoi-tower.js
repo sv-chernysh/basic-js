68ff9cc20873a813540199949fa02a3f28f0a91d
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 22:10:40 +0200

    feat: finish task Tower of Hanoi

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,4 +1,9 @@
-module.exports = function calculateHanoi(/* disksNumber, turnsSpeed */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
-}
+module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
+    let objHanoi = {
+        turns: 0,
+        seconds: 0
+    };
+    objHanoi.turns = Math.pow(2, disksNumber) - 1;
+    objHanoi.seconds = objHanoi.turns / (turnsSpeed / 3600);
+    return objHanoi;
+};

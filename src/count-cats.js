e162f9416ef2b2b984e72adbc0999896740f04da
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 18:48:36 +0200

    feat: finish task count cats

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,5 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let arrayWithCats = matrix.reduce((result, current) => result.concat(current), []);
+  let result = arrayWithCats.filter((item) => item === '^^');
+  return result.length;
 };

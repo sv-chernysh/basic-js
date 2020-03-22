e3123681bbe156435c7039fbf660a18020b6d5ad
Serhii Chernysh <srg.chernysh@gmail.com>
2020-03-22 20:32:07 +0200

    feat: finish task carbon dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,11 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  let k = 0.693 / HALF_LIFE_PERIOD;
+  let sample = parseFloat(sampleActivity);
+  if (typeof sampleActivity !== 'string' || sample > MODERN_ACTIVITY || sample <= 0 || isNaN(sample)) {
+    return false;
+  }
+  return Math.ceil(Math.log(MODERN_ACTIVITY / sample) / k);
 };
